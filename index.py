b3c5938c745f86568b5ed166efa9c88ddd95be06
Anna He <jxh604@case.edu>
2017-03-01 23:57:26 +0800

    key as nodes, and sorted posting

--- index.py
+++ index.py
@@ -2,6 +2,7 @@
 from nltk import word_tokenize
 import string
 import os
+from node import Node
 
 # Find list of unique tokens in all file path with respect to its document ID
 def process_documents(file_path):
@@ -11,7 +12,8 @@ def process_documents(file_path):
             continue
         new_file_path = file_path + filename
         tables.append(process_document(new_file_path, int(filename)))
-    print merge_tables(tables)
+    dictionary = merge_tables(tables)
+    printDict(dictionary)
 
 def process_document(file, doc_ID):
     content = open(file, "r").read()
@@ -36,9 +38,17 @@ def merge_tables(table_array):
     return sort_dictionary(dictionary)
 
 def sort_dictionary(dictionary):
+    new_dictionary = dict()
     for key in dictionary:
         posting = dictionary[key]
-        dictionary[key] = sorted(posting)
-    return dictionary
+        cur_node = Node(key, len(posting))
+        #dictionary[key] = sorted(posting)
+        new_dictionary[cur_node] = sorted(posting)
+    return new_dictionary
+
+def printDict(dictionary):
+    for key in dictionary:
+        k = key.token + ", " + str(key.frequency)
+        print k, dictionary[key]
 
 process_documents("reuters/training/")

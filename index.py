b317d6bb4cdd07bf0adc13d9b5d095e2d09c75da
Anna He <jxh604@case.edu>
2017-03-01 22:24:18 +0800

    basic indexing

---
+++ index.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+from nltk import word_tokenize
+import string
+import os
+
+# Find list of unique tokens in all file path with respect to its document ID
+def process_documents(file_path):
+    tables = []
+    for filename in os.listdir(file_path):
+        if filename == ".DS_Store":
+            continue
+        new_file_path = file_path + filename
+        tables.append(process_document(new_file_path, filename))
+    print merge_tables(tables)
+
+def process_document(file, doc_ID):
+    content = open(file, "r").read()
+    punctuations = list(string.punctuation)
+    result = [i for i in word_tokenize(content) if i not in punctuations]
+
+    table = dict()
+
+    for word in result:
+        if word not in table:
+            table[word] = doc_ID
+
+    return table
+
+def merge_tables(table_array):
+    dictionary = dict()
+    for table in table_array:
+        for key in table:
+            if key not in dictionary:
+                dictionary[key] = []
+            dictionary[key].append(table[key])
+    return dictionary
+
+process_documents("reuters/training/")
